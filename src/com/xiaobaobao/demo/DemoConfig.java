a23bf43d0e3b7c4631cf4a407f2c56d19d12c686
xiaobaobao007 <932824098@qq.com>
2020-03-27 17:35:35 +0800

    打war包

--- src/com/xiaobaobao/demo/DemoConfig.java
+++ src/com/xiaobaobao/demo/DemoConfig.java
@@ -18,7 +18,7 @@ public class DemoConfig extends JFinalConfig {
 	//http://localhost/bmy/login?a=root&b=root
 	//http://localhost/bmy/logout/root-root
 	public static void main(String[] args) {
-		JFinal.start("WebRoot", 80, "/", 5);
+		JFinal.start("WebRoot", 8080, "/", 5);
 	}
 
 	/**
